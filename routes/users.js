810923b96f4d5c85c6944bec500f56f23ae71c47
David Hull <david.hull37@gmail.com>
2020-03-29 17:23:39 -0500

    Changed code format

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const app = express();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+app.get('/', (req, res) => {
+  return res.send('respond with a resource');
 });
 
-module.exports = router;
+module.exports = app;
